4aa0d644473917958e457d61ae5fe4e7b77b74a0
liwei <liwei@entropyreduce.com>
2020-10-29 13:32:33 +0800

    鼠标绘点

--- HelloPoint/main.js
+++ HelloPoint/main.js
@@ -1,13 +1,19 @@
-
-// 顶点着色器
+/**
+ * 在该示例中
+ * 顶点着色器指定了点的位置和尺寸
+ * 片元着色器指定了顶点的颜色
+ */
+// 顶点着色器, 描述顶点特性
 const VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  attribute float a_PointSize;
   void main() {
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-    gl_PointSize = 10.0;
+    gl_Position = a_Position;
+    gl_PointSize = a_PointSize;
   }
 `;
 
-// 片元着色器
+// 片元着色器， 处理像素
 const FSHADER_SOURCE = `
   void main() {
     gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
@@ -29,9 +35,43 @@ function main() {
     return;
   }
 
+  // 获取a_Position的存储位置
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.error("无法找到attribute变量");
+    return;
+  }
+
+   // 获取a_Position的存储位置
+   var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+   if (a_PointSize < 0) {
+     console.error("无法找到a_PointSize attribute变量");
+     return;
+   }
+  gl.vertexAttrib1f(a_PointSize, 20.0);
+
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  canvas.addEventListener('click', e => click(e, gl, canvas, a_Position));
+}
+
+const gPoints = [];
+function click(e, gl, canvas, a_Position) {
+  let x = e.clientX;
+  let y = e.clientY;
+  const rect = e.target.getBoundingClientRect();
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+  gPoints.push(x);
+  gPoints.push(y);
+
   // 清空颜色缓冲区 = 清空画布
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.POINTS, 0, 1);
+  const len = gPoints.length;
+  for (let i = 0; i < len; i+=2) {
+    gl.vertexAttrib3f(a_Position, gPoints[i], gPoints[i + 1], 0.0);
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
